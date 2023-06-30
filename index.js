9f46c34e4219182430261378f28bfdb10849c5e3
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-06-30 16:34:06 -0500

    changed index.js again and it nearly works

--- index.js
+++ index.js
@@ -1,123 +1,90 @@
-// initialize variables
 const inquirer = require('inquirer');
-const generateSVG = require('./lib/logoGenerator.js')
-const fs = require('fs')
+const fs = require('fs');
+const {Shape,Circle, Triangle, Square} = require('./lib/shapes');
 
-const userInput = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Please enter up to three characters for the logo text.',
-            validate: validateCharacters // validate the input is not greater than 3
-        },
-        {
-            type: 'input',
-            name: 'titleColor',
-            message: 'Please enter a color keyword or hexidecimal number (excluding #) for the color of the text.',
-            validate: validateColor // validate the input is a color keyword or valid hexidecimal
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'What shape would you like to use?',
-            choices: ['circle','triangle','square'],
-            default: 'circle',
-            validate: validateInput // validate the input is not empty
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Please enter a color keyword or hexidecimal number (excluding #) for the color of the shape.',
-            validate: validateColor // validate the input is a color keyword or valid hexidecimal
-        }
-    ])
-};
+class Svg {
+  constructor(){
+    this.textElement =''
+    this.shapeElement =''
+  }
+  render(){
+    return `version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"`
+  }
+  setText(text,color){
+    this.textElement= ` <text x="150" y="125" font-size="60" text-anchor="middle" fill=${color}>${text}</text>`
+  }
+  setShapeElement(shape){
+    this.shapeElement = shape.render()
+  }
+}
 
-/**
- * @validateCharacters
- * Ensures the input is less than or
- * equal to three characters
- */
-const validateCharacters = async (input) => {
-    // if input is valid, return true
-    if (input.length <= 3) {
-        return true;
-    } 
-    // else display a message to the user
-    return 'Please enter three characters or less.';
-};
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters:',
+    validate: function (input) {
+      return input.length <= 3;
+    },
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter text color (keyword or hexadecimal number):',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Select a shape:',
+    choices: ['circle', 'triangle', 'square'],
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter shape color (keyword or hexadecimal number):',
+  },
+];
 
-/**
- * @validateColor
- * Ensures the color is a valid
- * color keyword OR a valid 
- * hexidecimal number (3 or 6 characters)
- */
-const validateColor = async (color) => {
-    // initialize variables
-    let hexRegex = /^[a-fA-F0-9]{6}$|^[a-fA-F0-9]{3}$/,
-        keywordRegex = /^(aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgrey|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|grey|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgrey|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|lime|limegreen|linen|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen)$/i;
-    // if it is a valid hexadecimal color, return true
-    if (hexRegex.test(color)) {
-        return true;
-    } 
-    // else if it is a valid color keyword, return true
-    else if (keywordRegex.test(color.toLowerCase())) {
-        return true;
-    }
-    // else display a message to the user
-    return 'Please enter a valid color keyword or hexidecimal number.';
-};
 
-/**
- * @validateInput
- * Ensures the user has entered something
- */
-const validateInput = async (input) => {
-    // if user has entered data, return true
-    if (input) {
-        return true;
-    } 
-    // else display a message to the user
-    return 'You must enter the information.';
-};
-
-/**
- * @writeToFile
- * Writes the SVG data to a file
- */
 function writeToFile(fileName, data) {
-    // write data to file
-    fs.writeFile(fileName, data, (err) => {
-        // if error, show error in console log
-        if (err) {
-            console.error(err);
-        }
-        // console log a success message 
-        else {
-            console.log('Generated logo.svg');
-        }
-    });
-}
+  const folderPath = './examples/'
+  const svg = new Svg();
+  svg.setText(data.text, data.textColor);
+  
+  switch (data.shape) {
+    case 'circle':
+      svg.setShapeElement(new Circle(data.shapeColor));
+      break;
+    case 'triangle':
+      svg.setShapeElement(new Triangle(data.shapeColor));
+      break;
+    case 'square':
+      svg.setShapeElement(new Square(data.shapeColor));
+      break;
+    default:
+      console.error(`Invalid shape selected: ${data.shape}`);
+      return;
+  }
+  
+  const svgContent = `<svg ${svg.render()}>${svg.shapeElement}${svg.textElement}</svg>`;
 
-/**
- * @init
- * Runs on app load
- */
+  
+  fs.writeFile(folderPath + fileName, svgContent, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Logo generated successfully!');
+    }
+  });
+};
+
+// TODO: Create a function to initialize app
 function init() {
-    // get the data from the prompts
-    userInput().then(res => {
-        // pass the responses to the generate.js file to create the SVG
-        return generate(res);
-    }).then(data => {
-        // pass the returned data to the @writeToFile function
-        return writeToFile("logo.svg", data);
-    }).catch(err => {
-        // console log the error
-        console.log("Error: " + err)
-    });
-}
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    writeToFile('logo.svg', answers);
+  });
+};
 
-// initialize the application
+// // Function call to initialize app
 init();
